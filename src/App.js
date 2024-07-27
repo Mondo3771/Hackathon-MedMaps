d989d0d592684cbbe1e3620dd84a074266969c8c
Mondo3771 <kabelomrn@gmail.com>
2024-07-27 22:00:21 +0200

    Changed the issues

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
 };
 
 const UpdateClinic = () => {
-    fetch(`/api/Hospitals`, {
+    fetch(`/api/DailyUpdates`, {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json',
