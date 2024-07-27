fd1344d16d56ee1a8df4a419ec4a5a34902345f8
Mondo3771 <kabelomrn@gmail.com>
2024-07-27 16:42:11 +0200

    Add the Update Api

--- Apicalls.js
+++ Apicalls.js
@@ -14,3 +14,84 @@ const GetAllHospital = () => {
       console.error('Error:',error);
     })
   }
+
+// Kabelo Stuff
+  const formatTime = (date) => {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    // Pad the hours, minutes, and seconds with leading zeros, if necessary
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    // Format the time in the 'HH:mm:ss' format
+    return hours + ':' + minutes + ':' + seconds;
+};
+
+const open = new Date();
+open.setHours(8,0,0,0);
+const openingTime = formatTime(open);
+
+const close = new Date();
+close.setHours(14,0,0,0);
+const closingTime = formatTime(close);
+let data = {
+  id:6,
+  name :"Example Hospital" ,
+  address:"123 Street Ave",
+  tel :"123456789012" ,
+  openingTime: openingTime,
+  closingTime:closingTime,
+  isClinic:true,
+  public:true,
+  open24Hours:true,
+  email:"ecxample@gmail.com",
+  website:"example.com.ac.za",
+  key:"key15",
+  Specialties:"Orthopedics,Dermatology,Neurology"
+}
+
+const AddAHospital = ()=>{
+ fetch("/api/Hospitals", {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data), // replace 'data' with the actual data you want to send
+})
+.then(response => response.json())
+.then(data => console.log(data))
+.catch((error) => console.error('Error:', error));
+}
+
+
+
+data = {
+  id:7,
+  name:"Albertin Hospital",
+address:"123 Street Ave",
+tel :"123456789012" ,
+openingTime: '08:00:00',
+closingTime:'17:00:00',
+isClinic:true,
+public:true,
+open24Hours:true,
+email:"ecxample@gmail.com",
+website:"example.com.ac.za",
+key:"key15",
+Specialties:"Orthopedics,Dermatology,Neurology"
+};
+const UpdateAHospital = () => {
+  fetch('/api/Hospitals',{
+    method:'PUT',
+    headers:{
+      'Content-Type': 'application/json',
+    },
+    body:JSON.stringify(data),
+  })
+  .then(res => res.json())
+  .then(data => console.log(data))
+  .catch((error)=> console.error('Error:',error));
+}
