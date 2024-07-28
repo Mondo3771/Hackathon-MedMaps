7d40c487fca01ff751291c66f8fafcecbf08b47f
Nathan Joseph <nathanjoseph069@gmail.com>
2024-07-28 10:40:29 +0200

    merged

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,24 +1,3 @@
-// import React from 'react'
-// import Maps from '../../Components/Maps/Maps'
-// import Aside from '../../Aside/Aside'
-// import News from '../../Components/News/News'
-// import Header from '../../Components/Header/Header'
-// import { Wrapper } from './HomePage.styles'
-
-// const HomePage = () => {
-//   return (
-//     <>
-//     <Header></Header>
-//     <Wrapper>
-//     <Maps></Maps>
-//     </Wrapper>
-     
-//     </>
-  
-//   )
-// }
-
-// export default HomePage
 import "./HomePage.css";
 import { useState } from "react";
 import Maps from "../Components/Maps/Maps";
