10fb60352eddba7a52e63d058cb527e0647a7522
Mondo3771 <kabelomrn@gmail.com>
2024-07-28 16:12:34 +0200

    commented

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
+// import { useAuth0 } from "@auth0/auth0-react";
 
 import Maps from "./Components/Maps/Maps";
 import HomePage from "./Pages/HomePage";
 import ClinicProfile from "./Pages/ClinicProfile/ClinicProfile";
-import Announcements from "./Components/Announcements/Announcements";
-import MakeAnnouncement from "./Components/MakeAnnouncement/MakeAnnouncement";
-import { fetchLocalStorage } from "./Helpers/helpers";
+// import Announcements from "./Components/Announcements/Announcements";
+// import MakeAnnouncement from "./Components/MakeAnnouncement/MakeAnnouncement";
+// import { fetchLocalStorage } from "./helpers/helpers";
 import {
   createBrowserRouter,
   createRoutesFromChildren,
@@ -18,8 +18,8 @@ import {
 
 // import { fetchLocalStorage, setLocalStorage } from './helpers/helpers';
 
-import ProfileComp from "./Components/ProfileComp/ProfileComp";
-import Header from "./Components/Header/Header";
+// import ProfileComp from "./Components/ProfileComp/ProfileComp";
+// import Header from "./Components/Header/Header";
 import CreateClinic from "./Sign Up page/CreateClinic";
 
 function App() {
